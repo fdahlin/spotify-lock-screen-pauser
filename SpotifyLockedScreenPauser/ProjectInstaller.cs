f1743d3d4d003670a4ab623914006f7968b29273
Fredrick Dahlin <fredrick.dahlin@consid.se>
2017-08-03 08:49:02 +0200

    Add project files.

---
+++ SpotifyLockedScreenPauser/ProjectInstaller.cs
@@ -0,0 +1,39 @@
+﻿using System.ComponentModel;
+using System.Configuration.Install;
+using System.Diagnostics;
+
+namespace SpotifyLockedScreenPauser
+{
+    [RunInstaller(true)]
+    public partial class ProjectInstaller : Installer
+    {
+        public ProjectInstaller()
+        {
+            InitializeComponent();
+            var installer = FindInstaller(Installers);
+            if (installer != null)
+            {
+                installer.Log = "SpotifyLockedScreenPauserLog";
+                installer.Source = "SpotifyLockedScreenPauserSource";
+            }
+        }
+
+        EventLogInstaller FindInstaller(InstallerCollection installers)
+        {
+            foreach (Installer installer in installers)
+            {
+                if (installer is EventLogInstaller)
+                {
+                    return (EventLogInstaller)installer;
+                }
+
+                EventLogInstaller eventLogInstaller = FindInstaller(installer.Installers);
+                if (eventLogInstaller != null)
+                {
+                    return eventLogInstaller;
+                }
+            }
+            return null;
+        }
+    }
+}
---
+++ SpotifyLockedScreenPauser/SpotifyLockedScreenPauserService.cs
@@ -0,0 +1,66 @@
+﻿using SpotifyAPI.Local;
+using System.ServiceProcess;
+
+namespace SpotifyLockedScreenPauser
+{
+    public partial class SpotifyLockedScreenPauserService : ServiceBase
+    {
+        readonly SpotifyLocalAPI _spotify;
+        bool _wasPlaying;
+
+        public SpotifyLockedScreenPauserService()
+        {
+            InitializeComponent();
+            CanHandleSessionChangeEvent = true;
+            _spotify = new SpotifyLocalAPI();
+        }
+
+        protected override void OnSessionChange(SessionChangeDescription changeDescription)
+        {
+            base.OnSessionChange(changeDescription);
+
+            switch (changeDescription.Reason)
+            {
+                case SessionChangeReason.SessionLock:
+                    Pause();
+                    break;
+                case SessionChangeReason.SessionUnlock:
+                    Play();
+                    break;
+            }
+        }
+
+        bool ConnectToSpotifyIfRunning()
+        {
+            if (SpotifyLocalAPI.IsSpotifyRunning())
+            {
+                return _spotify.Connect();
+            }
+            return false;
+        }
+
+        void Pause()
+        {
+            if (ConnectToSpotifyIfRunning())
+            {
+                if (_spotify.GetStatus().Playing)
+                {
+                    _wasPlaying = true;
+                    _spotify.Pause();
+                }
+            }
+        }
+
+        void Play()
+        {
+            if (ConnectToSpotifyIfRunning())
+            {
+                if (_wasPlaying)
+                {
+                    _wasPlaying = true;
+                    _spotify.Play();
+                }
+            }
+        }
+    }
+}
